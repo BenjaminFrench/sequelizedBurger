e7ea297e10f84c6dca19d4498265469aea23f289
Ben French <ben.jminw@gmail.com>
2018-02-23 08:46:39 -0700

    Sequelized.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,23 +3,24 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger.js");
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burger.all(function (data) {
+    db.Burger.findAll({}).then( result => {
         var hbsObject = {
-            burgers: data
+            burgers: result
         };
-        console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.create("burger_name", req.body.name, function (result) {
-        // Send back the ID of the new quote
-        res.json({ id: result.insertId });
+    db.Burger.create({
+        burger_name: req.body.name,
+        devoured: false
+    }).then( dbBurger => {
+        res.json(dbBurger);
     });
 });
 
@@ -28,25 +29,25 @@ router.put("/api/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    burger.update(
-        { devoured: true },
-        { id: req.params.id },
-        function (result) {
-            if (result.changedRows === 0) {
-                // If no rows were changed, then the ID must not exist, so 404
-                return res.status(404).end();
-            }
-            res.status(200).end();
-
+    db.Burger.update({
+        devoured: true
+        }, {
+        where: {
+            id: req.params.id
         }
-    );
+    }).then( (dbBurger) => {
+        res.json(dbBurger);
+    });
 });
 
 // reset all burgers to not devoured
 router.get("/api/burgers/reset", function (req, res) {
-    burger.updateAll( { devoured: false }, function (result) {
-        // Send back the ID of the new quote
-        res.json('Success');
+    db.Burger.findAll().then( (results) => {
+        console.log(results);
+        results.forEach(element => {
+            element.update({devoured: false});
+        });
+        res.json("Success");
     });
 });
 
