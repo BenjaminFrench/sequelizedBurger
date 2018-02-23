d47fd650f7031080eee4e7e655fe732df0123f14
Ben French <ben.jminw@gmail.com>
2018-02-23 08:11:28 -0700

    Sequelize.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var db = require("./models");
+
 var PORT = process.env.PORT || 3000;
 
 var app = express();
@@ -25,6 +27,8 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+  });
 });
